5fdfdc634403234d6a91b452fac09d821bfd6008
Will Bridges <106503676+wbridgri@users.noreply.github.com>
2026-04-20 19:40:00 -0500

    first commit

---
+++ src/ArraysTest.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author willbridges
+ */
+public class ArraysTest {
+    
+    public static void main(String[] args) {
+        
+//        java.util.Scanner input = new java.util.Scanner(System.in);
+//        
+//        double[] myList = new double[5];
+//        
+//        //initializes array with user inputted values
+//        System.out.print("Enter " + myList.length + " values: ");
+//        for(int i = 0; i < myList.length; i++) {
+//            myList[i] = input.nextDouble();
+//        }
+//        //prints myList array elements
+//        for(int i = 0; i < myList.length; i++)
+//            System.out.print(myList[i] + " ");
+        
+//        //sum of an array
+//        int[] myList2 = {1, 2, 3, 4, 5, 6};
+//        
+//        
+//        double total = 0;
+//        for(int i = 0; i < myList2.length; i++) {
+//            total += myList2[i];
+//            System.out.println(total + " ");
+//        }
+//        System.out.print(total);
+//       
+//        //Finding the largest element
+//        
+//        double[] myList = {1, 2, 3, 4, 5, 6};
+//        
+//        double max = myList[0];
+//        for(int i = 0; i < myList.length; i++) {
+//            if(myList[i] > max) max = myList[i];
+//        }
+//        System.out.print(max);
+//        
+//          //Find the smallest index of the largest element
+//          
+//          double max = myList[0];
+//          int indexOfMax = 0;
+//          for(int i = 1; i < myList.length; i++) {
+//              if(myList[i] > max) {
+//                  max = myList[i];
+//                  indexOfMax = i;
+//              }
+//          }
+//          System.out.print(indexOfMax);
+          
+
+        
+        
+        
+    }
+    
+    
+}
